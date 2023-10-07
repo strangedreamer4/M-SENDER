d56c42ad3a3af9d3fb5aa0dc6190191e223783e0
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2023-10-07 12:04:17 +0530

    Add files via upload

--- msender.py
+++ msender.py
@@ -1,16 +1,65 @@
+import socket
+import os
 import tkinter as tk
-import subprocess
+from tkinter import filedialog
+from tkinter import messagebox
 
-def launch_sender():
-    subprocess.Popen(["python", ".msender.py"])
+def send_file(receiver_host, receiver_port, file_path):
+    # Create a socket object
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def launch_receiver():
-    subprocess.Popen(["python", ".mreceiver.py"])
+    try:
+        # Connect to the receiver
+        s.connect((receiver_host, receiver_port))
+        
+        # Send the filename
+        filename = os.path.basename(file_path)
+        s.send(filename.encode())
+        
+        # Send the file data
+        with open(file_path, 'rb') as file:
+            while True:
+                data = file.read(1024)
+                if not data:
+                    break
+                s.send(data)
+        
+        messagebox.showinfo("Success", f"File '{filename}' sent successfully to {receiver_host}:{receiver_port}")
+    except Exception as e:
+        messagebox.showerror("Error", f"Error: {e}")
+    finally:
+        s.close()
 
-# Create the main window for the launcher with a dark theme and green text color
+def browse_file():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        file_path_entry.delete(0, tk.END)
+        file_path_entry.insert(0, file_path)
+
+def send_file_to_receiver():
+    receiver_host = receiver_host_entry.get()
+    receiver_port_str = receiver_port_entry.get()
+    file_path = file_path_entry.get()
+
+    if not receiver_host:
+        messagebox.showerror("Error", "Receiver Host cannot be empty.")
+        return
+
+    if not receiver_port_str.isdigit():
+        messagebox.showerror("Error", "Receiver Port must be a positive integer.")
+        return
+
+    receiver_port = int(receiver_port_str)
+
+    if not os.path.isfile(file_path):
+        messagebox.showerror("Error", "Invalid file path.")
+        return
+
+    send_file(receiver_host, receiver_port, file_path)
+
+# Create the main window for the sender
 root = tk.Tk()
-root.title("File Transfer Launcher")
-root.geometry("400x200")  # Set the window size
+root.title("File Sender")
 
 # Dark theme
 root.configure(bg="black")
@@ -18,11 +67,30 @@ root.configure(bg="black")
 # Green text color
 text_color = "green"
 
-# Create and arrange widgets for launcher
-sender_button = tk.Button(root, text="Launch Sender", command=launch_sender, fg=text_color, bg="black", relief="flat")
-sender_button.pack(pady=20)  # Add padding between the buttons
+# Create and arrange widgets for sender
+receiver_host_label = tk.Label(root, text="Receiver Host:", fg=text_color, bg="black")
+receiver_host_label.pack()
 
-receiver_button = tk.Button(root, text="Launch Receiver", command=launch_receiver, fg=text_color, bg="black", relief="flat")
-receiver_button.pack(pady=20)  # Add padding between the buttons
+receiver_host_entry = tk.Entry(root, width=50)
+receiver_host_entry.pack()
+
+receiver_port_label = tk.Label(root, text="Receiver Port:", fg=text_color, bg="black")
+receiver_port_label.pack()
+
+receiver_port_entry = tk.Entry(root, width=50)
+receiver_port_entry.pack()
+
+file_path_label = tk.Label(root, text="Select File to Send:", fg=text_color, bg="black")
+file_path_label.pack()
+
+file_path_entry = tk.Entry(root, width=50)
+file_path_entry.pack()
+
+browse_button = tk.Button(root, text="Browse", command=browse_file, fg=text_color, bg="black", relief="flat")
+browse_button.pack()
+
+send_button = tk.Button(root, text="Send File", command=send_file_to_receiver, fg=text_color, bg="black", relief="flat")
+send_button.pack()
 
 root.mainloop()
+
